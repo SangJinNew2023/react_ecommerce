907f992bec66389be82d94efa284f7a6e19f6aae
Sang jin An <photoan79@naver.com>
2023-05-24 19:48:00 -0700

    useReducer

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import HeroSection from "./components/HeroSection";
+import { useProductContext } from "./context/productcontext";
+
+const About = () => {
+  const { myName } = useProductContext()
+  const data = {
+    name: "thapa Ecommerce",
+};
+
+return(
+    <>
+      {myName}
+      <HeroSection myData={data}/>;
+    </>
+  ); 
+};
+
+export default About;
