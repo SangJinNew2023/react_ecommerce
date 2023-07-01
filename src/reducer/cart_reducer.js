082330b474f516519d44b90a657f67780160740a
Sang jin An <photoan79@naver.com>
2023-07-01 12:34:55 -0700

    51

--- src/reducer/cart_reducer.js
+++ src/reducer/cart_reducer.js
@@ -4,17 +4,41 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD_TO_CART") {
     let {id, color, amount, product } = action.payload;
 
-    let cartProduct;
-    
-    cartProduct = {
-      id : id + color,
-      name : product.name,
-      color,
-      amount,
-      image : product.image[0].rul,
-      price: product.price,
-      max: product.stock,
-    };
+    // takle the existing product
+    let existingProduct = state.cart.find(
+      (curItem) => curItem.id === id + color
+    );
+
+    if (existingProduct) {
+      let updateProduct = state.cart.map((curElem) =>{
+        if(curElem.id === id + color) {
+          let newAmount = curElem.amount + amount;
+          if(newAmount >= curElem.max) {
+            newAmount = curElem.max;
+          }
+          return {
+            ...curElem,
+            amount: newAmount,
+          };
+        } else {
+          return curElem;
+        };
+      });
+      return {
+        ...state,
+        cart: updateProduct,
+      };
+    } else {
+      let cartProduct = {
+        id : id + color,
+        name : product.name,
+        color,
+        amount,
+        image : product.image[0].rul,
+        price: product.price,
+        max: product.stock,
+      };
+    }
 
     return {
       ...state,
@@ -22,12 +46,20 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === "REMOVE_ITEM") {
-    let updatedCart = state.cart.filter((curItem) => curItem.id !== action.payload)
+    let updatedCart = state.cart.filter(
+      (curItem) => curItem.id !== action.payload)
     return {
       ...state,
-      cart: [],
+      cart: updatedCart,
     }
   };
+  //to empty or to clear to cart
+  if(action.type === "CLEAR_CART") {
+    return {
+      ...state,
+      cart:[],
+    }
+  }
   return state;
 }
 
